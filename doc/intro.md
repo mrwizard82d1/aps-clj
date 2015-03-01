d8a00706d84d45035407576d36efab5e504592bb
Larry Jones <mrwizard82d1@gmail.com>
2015-02-28 20:40:01 -0600

    Initial leiningen project structure.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to apatterns
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/apatterns/core_test.clj
@@ -0,0 +1,7 @@
+(ns apatterns.core-test
+  (:require [clojure.test :refer :all]
+            [apatterns.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
